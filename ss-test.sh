713359fa50f668630746612816608c165df2bff0
Thom-Glynn <78485658+Thom-Glynn@users.noreply.github.com>
2021-04-08 21:40:01 +0100

    Create ss-test.sh

---
+++ ss-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ssh ubuntu@35.176.144.11 << EOF
+
+
+sudo rm -r PracticalProject
+git clone https://github.com/Thom-Glynn/PracticalProject.git
+cd PracticalProject/
+docker-compose up -d --build
+docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
+docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
+docker-compose down
+EOF

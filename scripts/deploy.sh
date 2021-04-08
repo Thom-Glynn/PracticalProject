6c76ccb85f8ef0d22f17443d3e5352e9fb321b60
Thom-Glynn <78485658+Thom-Glynn@users.noreply.github.com>
2021-04-08 16:25:47 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,10 @@ cd kubernetes/
 kubectl delete pods --all -n cne
 kubectl apply -f namespace.yaml
 kubectl apply -f nginx-conf.yaml
-kubectl apply -f nginx-lb.yaml -f nginx.yaml -f flask-app.yaml -f flask-cip.yaml -f backend-cip.yaml -f backend.yaml
+kubectl apply -f nginx-lb.yaml
+kubectl apply -f nginx.yaml
+kubectl apply -f flask-app.yaml
+kubectl apply -f flask-cip.yaml
+kubectl apply -f backend-cip.yaml
+kubectl apply -f backend.yaml
 kubectl describe service nginx-lb -n cne

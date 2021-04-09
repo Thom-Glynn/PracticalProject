4578ecd6c7ab3e3c631de79bc38c97b0f8879c44
Thom-Glynn <78485658+Thom-Glynn@users.noreply.github.com>
2021-04-09 10:57:29 +0100

    Update ss-test.sh

--- ss-test.sh
+++ ss-test.sh
@@ -5,6 +5,7 @@ ssh -tt -o StrictHostKeyChecking=no ubuntu@18.132.2.194 << EOF
 sudo rm -r PracticalProject
 git clone https://github.com/Thom-Glynn/PracticalProject.git
 cd PracticalProject/
+git checkout main
 docker-compose up -d --build
 docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
 docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
